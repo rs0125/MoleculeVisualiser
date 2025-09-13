08f04e1c33a12dd8775d8b1274fdbcb404eae5cb
Raghav Sharma <raghaaav5@gmail.com>
2025-09-13 20:30:20 +0530

    feat: configured grabbables, updated scene manager

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -1,41 +1,86 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// This class handles loading all the different scenes in the project.
+/// Attach this script to a GameObject in your scene (like a GameManager or UI Canvas).
+/// Then, you can link UI buttons' OnClick() events to the public functions below.
+/// </summary>
 public class SceneController : MonoBehaviour
 {
-    // Function to load a specific scene by name
+    /// <summary>
+    /// A generic function to load any scene by its name.
+    /// </summary>
+    /// <param name="sceneName">The exact name of the scene to load.</param>
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
     }
 
-    // Function to load Scene 1
+    // --- Functions to load specific scenes ---
+
+    public void LoadBoric()
+    {
+        SceneManager.LoadScene("Boric");
+    }
+
+    public void LoadCO2()
+    {
+        SceneManager.LoadScene("CO2");
+    }
+
+    public void LoadCube()
+    {
+        SceneManager.LoadScene("Cube");
+    }
+
+    public void LoadCyclohexane()
+    {
+        SceneManager.LoadScene("Cyclohexane");
+    }
+
+    public void LoadDifluoroDiazene()
+    {
+        SceneManager.LoadScene("DiflouroDiazene");
+    }
+
+    public void LoadEth_ChloBro()
+    {
+        SceneManager.LoadScene("Eth_ChloBro");
+    }
+
+    public void LoadFullerene()
+    {
+        SceneManager.LoadScene("Fullerene");
+    }
+
     public void LoadH2O()
     {
         SceneManager.LoadScene("H2O");
     }
 
-    // Function to load Scene 2
-    public void LoadNH3()
-    {
-        SceneManager.LoadScene("NH3");
-    }
-
-    // Function to load Scene 3
     public void LoadHCN()
     {
         SceneManager.LoadScene("HCN");
     }
 
-    // Function to load Scene 4
-    public void LoadCH4()
+    public void LoadMethane()
     {
-        SceneManager.LoadScene("CH4");
+        SceneManager.LoadScene("Methane");
     }
 
-    // Function to load Scene 5
-    public void LoadBoric()
+    public void LoadNH3()
     {
-        SceneManager.LoadScene("Boric");
+        SceneManager.LoadScene("NH3");
+    }
+
+    public void LoadSOCL()
+    {
+        SceneManager.LoadScene("SOCL");
+    }
+
+    public void LoadTetraCobalt()
+    {
+        SceneManager.LoadScene("TetraCobalt");
     }
 }
